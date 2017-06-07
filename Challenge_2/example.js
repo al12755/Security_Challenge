9765a5d56383cfbb1c8f8fcf18bb80be3a1f5b72
adamlyth <adamdavidlyth@gmail.com>
2017-06-07 16:46:15 +0100

    💭 Added Comments

--- Challenge_2/example.js
+++ Challenge_2/example.js
@@ -1,8 +1,14 @@
+// These commands execute the challenge binary for you
+// You may need to change challenge-macos to your relevant OS
 const exec = require('child_process').exec;
 const cmd = function(string) {
   return `./challenge-macos ${string}`;
 }
 
+// This function will run the attempt for you
+// True: correct password
+// False: incorrect password
+// Don't worry about it just call it your password attempt!
 async function attempt(string) {
   return new Promise(function(resolve, reject) {
     exec(cmd(string), function(error, stdout, stderr) {
@@ -16,6 +22,7 @@ async function attempt(string) {
 async function solution() {
   const a = await attempt('Attempt');
   console.log(a);
+  // ...Code here
 }
 
 solution();
